58713406e00426db5c5d59d92eb36dfcb99350af
zisgood <33769446+zisgood@users.noreply.github.com>
2022-04-01 18:20:45 +0800

    Delete docs directory

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from 'D:/Code/DocumentProjects/blog/node_modules/@mr-hope/vuepress-plugin-components/lib/client/root-components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
