546cb35f57477a7860d209e72a5a94f19fcca32b
zisgood <zisgood@gitee.com>
2025-02-23 19:08:16 +0800

    push

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -7,7 +7,7 @@ export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
   author: {
-    name: "Mr.Hope",
+    name: "zisgood",
     url: "https://mister-hope.com",
   },
 
