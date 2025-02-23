5cabd63f58ace2828649ddc920e3fecb2897f7ac
zisgood <zisgood@gitee.com>
2025-02-23 18:07:38 +0800

    push

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+  title: "zisgood的笔记",
+  description: "zisgood的笔记",
 
   theme,
 
